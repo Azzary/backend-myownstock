470258020c92aa537ede34976538b9e5c8333229
Remi colin <s35387@stagiaire.aelion.fr>
2024-01-10 14:32:38 +0100

    Add update delete and get 1 product, add validator et logique in the service

--- src/main/java/org/aelion/product/product/Product.java
+++ src/main/java/org/aelion/product/product/Product.java
@@ -1,21 +1,27 @@
 package org.aelion.product.product;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class Product {
 
+    @NotNull(message = "Id cannot be null")
+    @NotBlank(message = "Id cannot be blank")
     private String id;
 
+    @NotNull(message = "Label cannot be null")
+    @NotBlank(message = "Label cannot be blank")
     private String label;
 
-    private  int stock;
+    @Min(value = 0, message = "The value must be positive")
+    private int stock;
 
     public void setStock(int stock) {
         if(stock < 0) throw new IllegalArgumentException("Stock cannot be negative");
--- src/main/java/org/aelion/product/product/ProductController.java
+++ src/main/java/org/aelion/product/product/ProductController.java
@@ -3,6 +3,7 @@ package org.aelion.product.product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -21,28 +22,20 @@ public class ProductController {
     }
 
     // get product by id
-    @GetMapping("/{id}")
-    public Product getProduct(@PathVariable String id) {
-        return service.findAll().stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
+    @GetMapping("/product")
+    public @ResponseBody Product getProduct(@Validated @RequestBody String id) {
+        return service.getProductById(id);
     }
 
     //remove product by id
-    @DeleteMapping("/{id}")
-    public void removeProduct(@PathVariable String id) {
-        service.findAll().stream().filter(p -> p.getId().equals(id)).findFirst().ifPresent(p -> {
-            service.findAll().remove(p);
-        });
+    @DeleteMapping
+    public @ResponseBody void removeProduct(@Validated @RequestBody String id) {
+        service.removeProductById(id);
     }
 
     @PutMapping
-    public @ResponseBody ResponseEntity add(Product product) {
-        if(product.getStock() < 0) throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Stock cannot be negative");
-        if (product.getId() == null || product.getId().isEmpty()) throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id cannot be empty");
-        if (product.getLabel() == null || product.getLabel().isEmpty()) throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "Label cannot be empty");
+    public void add(@Validated @RequestBody Product product) {
         service.AddProduct(product);
-        // return status 201
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-
     }
 
 
